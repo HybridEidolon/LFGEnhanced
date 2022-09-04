3595112ac99214aa78df6f3b40bd1d2426b0b31f
Eidolon <furyhunter600@gmail.com>
2022-09-04 00:29:48 -0500

    Split scanning behavior between solo and party

--- LFGEnhanced.lua
+++ LFGEnhanced.lua
@@ -26,6 +26,29 @@ local function ActivateEntryAutoSearch()
 	ResetAutoSearchDeadline()
 end
 
+-- Partially-applicable (curried) table filter function
+local function filter(predicate)
+	return function(table)
+		local newtab = {}
+		for _, v in ipairs(table) do
+			if predicate(v) then
+				newtab[#newtab+1] = v
+			end
+		end
+		return newtab
+	end
+end
+
+local FilterSoloEntries = filter(function (search_result_id)
+	local result_info = C_LFGList.GetSearchResultInfo(search_result_id)
+	if result_info == nil then
+		return false
+	end
+
+	local result_members = result_info.numMembers
+	return result_members == 1
+end)
+
 -- This function can only be called for HW execution contexts.
 local function AutoSearchCurrentEntry()
 	ResetAutoSearchDeadline()
@@ -47,34 +70,29 @@ local function AutoSearchCurrentEntry()
 		end
 	end
 
+	-- In the DBC, categories 2 and 117 are Dungeons and Heroic Dungeons.
+	-- Raids are 114. Maybe for later...
+
 	if type(category_id) == "number" then
 		C_LFGList.Search(category_id, active_entry.activityIDs)
 	end
 end
 
--- Process the current LFGList search results.
-local function DisplayMessagesForCurrentSearchResults()
-	local result_count, results = C_LFGList.GetFilteredSearchResults()
+-- Display system messages for a solo entry in the LFG List.
+local function DisplaySoloEntryMessages()
+	local _, results = C_LFGList.GetFilteredSearchResults()
 
-	if not C_LFGList.HasActiveEntryInfo() then
-		-- Don't do anything unless there is an active LFG entry.
-		return
-	end
-
-	local active_entry = C_LFGList.GetActiveEntryInfo()
-
-	local group_member_counts = GetGroupMemberCounts()
-
+	-- This assumes the standard Tank, Healer, 3 DPS party configuration
 	local looking_for_your_roles = 0
-	local greater_than_1 = 0
 	local your_roles = C_LFGList.GetRoles()
+
 	for _, v in ipairs(results) do
 		local result_info = C_LFGList.GetSearchResultInfo(v)
 		local result_members = result_info.numMembers
 		local member_counts = C_LFGList.GetSearchResultMemberCounts(v)
 		local group_needs_you = false
 
-		if member_counts ~= nil then
+		if member_counts ~= nil and result_members < 5 then
 			if your_roles.tank == true and member_counts.TANK == 0 then
 				group_needs_you = true
 			end
@@ -91,14 +109,92 @@ local function DisplayMessagesForCurrentSearchResults()
 		if group_needs_you then
 			looking_for_your_roles = looking_for_your_roles + 1
 		end
-
-		if result_members > 1 then
-			greater_than_1 = greater_than_1 + 1
-		end
 	end
 
 	if looking_for_your_roles > 0 then
-		SendSystemMessage(format("LFG: %d Groups. %d need your roles. %d have more than 1 member.", result_count, looking_for_your_roles, greater_than_1))
+		SendSystemMessage(format(
+			"LFG: %d Results are looking for your selected roles.",
+			looking_for_your_roles
+		))
+	else
+		SendSystemMessage("LFG: No candidate Results.")
+	end
+end
+
+-- Display system messages for a party entry in the LFG List.
+-- Selects all solo entries matching the search criteria and checks their listed
+-- roles. If your party needs one of their roles, it is included in the count.
+local function DisplayPartyEntryMessages()
+	local _, results = C_LFGList.GetFilteredSearchResults()
+
+	-- {DAMAGER, TANK, HEALER, NOROLE}
+	local member_counts = GetGroupMemberCounts()
+
+	if member_counts == nil then
+		-- blizzard api bug?
+		error("member_counts = nil")
+		return
+	end
+
+	-- PartyUtil GetGroupMemberCountsForDisplay says NOROLE = DAMAGER
+	local need_tank = 1 - member_counts.TANK
+	local need_healer = 1 - member_counts.HEALER
+	local need_dps = 3 - (member_counts.DAMAGER + member_counts.NOROLE)
+
+	local available_candidates = 0
+	local available_tanks = 0
+	local available_healers = 0
+	local available_dps = 0
+
+	for _, v in ipairs(FilterSoloEntries(results)) do
+		local member_counts = C_LFGList.GetSearchResultMemberCounts(v)
+		local include_entry = false
+
+		if need_tank > 0 and member_counts.TANK > 0 then
+			include_entry = true
+			available_tanks = available_tanks + 1
+		end
+		if need_healer > 0 and member_counts.HEALER > 0 then
+			include_entry = true
+			available_healers = available_healers + 1
+		end
+		if need_dps > 0 and member_counts.DAMAGER > 0 then
+			include_entry = true
+			available_dps = available_dps + 1
+		end
+
+		if include_entry then
+			available_candidates = available_candidates + 1
+		end
+	end
+
+	if available_candidates > 0 then
+		-- Maybe this should play a sound and highlight the LFG minimap eye?
+		SendSystemMessage(format(
+			"LFM: %d Results (%d T/%d H/%d D).",
+			available_candidates,
+			available_tanks,
+			available_healers,
+			available_dps
+		))
+	else
+		SendSystemMessage("LFM: No candidate Results.")
+	end
+end
+
+-- Process the current LFGList search results.
+local function DisplayMessagesForCurrentSearchResults()
+	if not C_LFGList.HasActiveEntryInfo() then
+		-- Don't do anything if there is not an active LFG entry.
+		return
+	end
+
+	local total_group_members = GetNumGroupMembers(LE_PARTY_CATEGORY_HOME)
+
+	if total_group_members < 2 then
+		DisplaySoloEntryMessages()
+	else
+		DisplayPartyEntryMessages()
 	end
 end
 
